a9961592b8d5fb83f87028b68cbdf0c6dfc688c2
Doğukan AKAY <akaydogukan@outlook.com>
2022-08-24 04:58:53 +0300

    EntityFramework Added.

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Customer : IEntity
+    {
+        //Northwind veritabanında customer id string olarak tutulduğu için bu projede customer id'yi string olarak aldık.
+        public string CustomerId { get; set; }
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+
+    }
+}
