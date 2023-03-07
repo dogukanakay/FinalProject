80a9282579211c8bbe7bbe43d7fee85dee176c10
akayd <akayd@ABRA>
2023-03-07 17:34:05 +0300

    dbcontext degisti

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -15,7 +15,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database= Northwind; Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server=ABRA; Database= Northwind; Trusted_Connection=true");
         }
 
         public DbSet<Product> Products { get; set; }
