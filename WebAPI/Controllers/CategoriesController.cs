51e196599b8608c5da41d627c2c77263e0dedb02
akayd <akayd@ABRA>
2023-05-01 03:36:15 +0300

    18

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+
+            //Dependency chain -- Bağımlılık zinciri  => KÖTÜ KOD BUNU YAPMA -- Alttaki 3 satır kod
+            //IProductService productService = new ProductManager(new EfProductDal());
+            //var result = productService.GetAll();
+            //return result.Data;
+        }
+    }
+}
